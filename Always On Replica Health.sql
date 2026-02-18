0a02dbda25204d5d384842946c29c3a8f276d7f9
Winty <wintyboss@outlook.com>
2026-02-18 03:53:46 -0800

    Added AO check

---
+++ Always On Replica Health.sql
@@ -0,0 +1,14 @@
+SELECT 
+		ag.name AS ag_name, 
+		ar.replica_server_name, 
+		rs.role_desc,
+		rs.connected_state_desc, 
+		rs.synchronization_health_desc 
+		FROM
+			sys.availability_groups ag 
+		JOIN sys.availability_replicas ar ON ag.group_id = ar.group_id 
+		JOIN sys.dm_hadr_availability_replica_states rs ON ar.replica_id = rs.replica_id 
+		ORDER BY 
+				ag.name,
+				ar.replica_server_name
+	;
